912ae6d5dfabd14ab413c55f8b8e76a49def894b
Alex <alexcfurtuna@gmail.com>
2017-06-27 10:23:47 +0100

    send grid installed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,19 @@
 class StaticPagesController < ApplicationController
+
   def index
-    
   end
 
   def landing_page
     @products = Product.limit(3)
   end
 
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: email,
+        to: 'alexcfurtuna@gmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+      end
 end
