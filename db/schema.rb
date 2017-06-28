272a98420bff3920aac439896b83185e3eb982da
Alex <alexcfurtuna@gmail.com>
2017-06-28 14:53:12 +0100

    5.4 issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419145913) do
+ActiveRecord::Schema.define(version: 20170628113958) do
+
+  create_table "first_names", force: :cascade do |t|
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
