da72bc51ec5dc990a2c1f79e1ddc638899a5c318
Alex <alexcfurtuna@gmail.com>
2017-06-30 15:21:30 +0100

    5.4 issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,4 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
 
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+config.action_mailer.default_url_options = { host: 'localhost:3000' }
