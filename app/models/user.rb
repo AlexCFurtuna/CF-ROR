f2ad86b2e6543295057fa06c6c49d887b6301514
Alex <alexcfurtuna@gmail.com>
2017-07-05 15:01:30 +0100

    5.4 issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   has_many :orders
+
+  after_create :send_admin_mail
+  def send_admin_mail
+    UserMailer.send_new_user_message(self).deliver
+  end
 end
