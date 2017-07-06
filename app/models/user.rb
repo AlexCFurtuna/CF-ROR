1b4f13502b52b92ca9245825be7d65be43b25f05
Ben Bagley <ben@benbagley.co.uk>
2017-07-06 18:58:16 +0100

    added confirmable and welcome email on signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable
+
   has_many :orders
-
-  after_create :send_admin_mail
-  def send_admin_mail
-    UserMailer.send_new_user_message(self).deliver
-  end
-end
-
-class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  load_and_authorize_resource
 end
