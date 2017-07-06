c37000dfb7de38b1e34f62e968313c43baa3f6fe
Alex <alexcfurtuna@gmail.com>
2017-07-06 20:43:46 +0100

    5.4 mailing issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+  devise :database_authenticatable, :registerable, :confirmable,
+     :recoverable, :rememberable, :trackable, :validatable,
+     :lockable
   has_many :orders
 end
