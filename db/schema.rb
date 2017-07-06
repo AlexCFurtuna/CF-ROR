cbcfceb774d1d47541e7dfbb130e2f8b06141633
Alex <alexcfurtuna@gmail.com>
2017-07-06 21:30:23 +0100

    5.4

--- db/schema.rb
+++ db/schema.rb
@@ -52,8 +52,10 @@ ActiveRecord::Schema.define(version: 20170630154228) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
