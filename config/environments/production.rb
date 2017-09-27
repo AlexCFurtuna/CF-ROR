5b6851e70bcd6d6a53ff2289a7835c8e98cf3a39
Alex <alexcfurtuna@gmail.com>
2017-09-27 22:27:16 +0300

    issues with assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,9 +25,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.digest = true
-  config.serve_static_files = true
   config.assets.initialize_on_precompile = false
-
+  config.public_file_server.enabled = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
