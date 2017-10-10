73b774f3cc3138421b410074d826d359092fe76f
Alex <alexcfurtuna@gmail.com>
2017-10-11 00:49:51 +0100

    6.2/average rating test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,4 +1,17 @@
-require 'rails_helper'
+require "rails_helper"
+
 describe Product do
-  let:(product) {Product.create!(name: "race bike") }
+
+  context 'when the product has comments' do
+
+    before do
+      @product.comments.create!(rating: 1, user: user, body: "Awful bike!")
+      @product.comments.create!(rating: 3, user: user, body: "Ok bike!")
+      @product.comments.create!(rating: 5, user: user, body: "Great bike!")
+    end
+
+    it "returns the average rating of all comments" do
+      expect(@product.average_rating).to eq(3)
+    end
+  end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+  describe User do
+    let(:user) { User.new(name: "Alex Trubard", email: nill, password: nill) }
+  end
