8b2a3177d3212d055b2cc215daaa44fd56b3d187
Alex <alexcfurtuna@gmail.com>
2017-10-11 01:28:18 +0100

    6.2/ average rating test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,10 +1,15 @@
 require "rails_helper"
 
 describe Product do
-
   context 'when the product has comments' do
 
     before do
+      @product = Product.create!(name: "race bike")
+      @user = User.create!(first_name: "BDD", last_name: "TEST", email: "test1@test.com", password: "test1234")
+
+      user = User.first
+      user.confirm
+
       @product.comments.create!(rating: 1, user: user, body: "Awful bike!")
       @product.comments.create!(rating: 3, user: user, body: "Ok bike!")
       @product.comments.create!(rating: 5, user: user, body: "Great bike!")
