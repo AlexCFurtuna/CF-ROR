63e4ee7a03b2a3d47eb41df0b22d485e42613cf9
Alex <alexcfurtuna@gmail.com>
2017-10-19 16:13:14 +0100

    6.4 FactoryGirl

--- spec/factories/user_factories.rb
+++ spec/factories/user_factories.rb
@@ -1,9 +1,16 @@
 FactoryGirl.define do
+  sequence(:email) { |n| "user#{n}@example.com" }
+
   factory :user do
-    email "anna@ferguson.com"
-    password "12344321"
-    first_name "Anna"
-    last_name "Ferguson"
+    email
+    password "123454321"
+    first_name "User"
+    last_name "Example"
     admin false
+  end
+
+  factory :admin, parent: :user do
+    id
+    admin true
   end
 end
