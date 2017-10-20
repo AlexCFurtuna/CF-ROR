6d3bff30082c10bdef05a52b661f4fd662161503
Alex <alexcfurtuna@gmail.com>
2017-10-20 17:53:00 +0100

    6.5

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,13 @@
+if Rails.env.production?
+  Rails.confirguration.stripe = {
+    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+    secret_key: ENV['STRIPE_SECRET_KEY']
+  }
+else
+  Rails.configuration.stripe = {
+    publishable_key: 'pk_test_FtAgNpVpDq9EHwJpfADb36ao',
+    secret_key: 'sk_test_xc1fuPh5G8nYFMns437tz2OJ'
+  }
+end
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
