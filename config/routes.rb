1ad6f08a26a10d27a7807020785db12ed5e544ee
Alex <alexcfurtuna@gmail.com>
2017-10-24 16:07:06 +0100

    6.6 / fixing product_path error

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout' }
+
+  devise_for :users, path: '',
+   path_names: {sign_in: 'login', sign_out: 'logout' }
 
   resources :orders, only: [:index, :show, :create, :destroy]
 
   resources :products do
     resources :comments
   end
-  
+
   resources :users
 
   get 'static_pages/about'
   get 'static_pages/contact'
   get 'static_pages/index'
+  get 'static_pages/landing_page'
+  get 'payments/create'
+
+  post 'static_pages/thank_you'
+  post 'payments/create'
 
   root 'static_pages#index'
 
-  post 'static_pages/thank_you'
+
 end
