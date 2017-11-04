286ee279d139699de258401963bd226a1335af4b
Alex <alexcfurtuna@gmail.com>
2017-11-04 00:48:02 +0000

    6.12

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
 
 module RorApp
   class Application < Rails::Application
