f7fc083062daeabcab9113341a56e06c31d0feee
Alex <alexcfurtuna@gmail.com>
2017-11-05 00:44:04 +0000

    fixing errors

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -17,6 +17,12 @@ class UserMailer < ApplicationMailer
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    mail(:to => 'alexcfurtuna@gmail.com', :subject => "Your order has been placed! You have been charged #{@product.price} for the #{@product.name}. You will be receiving the product soon. Thank you for you purchase!")
+    mail(:to => 'alexcfurtuna@gmail.com', :subject => "Welcome to Mury Art!")
+  end
+
+  def order_placed(user, product)
+    @user = user
+    @product = product
+    mail(:to => @user.mail, :subject => "Your order has been placed! You have been charged #{@product.price} for the #{@product.name}. You will be receiving your item soon. Thank you for your purchase!")
   end
 end
