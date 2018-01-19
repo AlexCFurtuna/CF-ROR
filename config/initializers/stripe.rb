7a4d5a51d032cef38c6b4a90dd05fcd0b34916ea
Alex <alexcfurtuna@gmail.com>
2018-01-19 21:29:45 +0000

    fixing errors

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,14 +4,11 @@ if Rails.env.production?
     secret_key: ENV['STRIPE_SECRET_KEY']
   }
 
-  private
-
  else
   Rails.configuration.stripe = {
     publishable_key: 'pk_test_FtAgNpVpDq9EHwJpfADb36ao',
     secret_key: 'sk_test_xc1fuPh5G8nYFMns437tz2OJ'
   }
  end
-end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
